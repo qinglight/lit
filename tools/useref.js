666886d43ad43f1b19e2a93400fa76705806c1d3
Abel <sdwyub@qq.com>
2016-03-21 16:25:15 +0800

    调整代码

---
+++ tools/useref.js
@@ -0,0 +1,40 @@
+var through = require('through2');
+var gutil = require('gulp-util');
+var PluginError = gutil.PluginError;
+var _ = require('../kernel').util;
+var useref = require('useref');
+var vfs = require('vinyl-fs');
+var concat = require('gulp-concat');
+var gulpif = require('gulp-if');
+
+const PLUGIN_NAME = 'gulp-light-useref';
+
+function template(options) {
+  var searchPath = options.searchPath;
+
+  return through.obj(function(file, enc, cb) {
+    var output = useref(file.contents.toString(), options);
+
+    file.contents = new Buffer(output[0]);
+    this.push(file);
+
+    var that = this;
+    _.map(output[1],function(type){
+      _.map(type,function(files,name){
+        files = files.assets;
+        var pattern = "{"+searchPath.join(",")+"}/"+"{"+files.join(",")+"}";
+        var src = vfs.src(pattern)
+
+        src
+          .pipe(gulpif(!options.noconcat, concat(name)))
+          .pipe(through.obj(function (newFile, encoding, callback) {
+              self.push(newFile);
+              // console.log(new File())
+              callback();
+          }));
+      })
+    })
+  });
+}
+
+module.exports = template;
