77037a5817f8d756945fd8bcb4e9b10ad3dc5969
wyub <sdwyub@qq.com>
2016-09-19 19:27:44 +0800

    基本功能搞完，自测准备发布

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ function help() {
 	console.log('  gen\t\t代码生成');
 	console.log('  release\t代码的集成打包');
 	console.log('  server\t内置HTTP服务器');
+	console.log('  plugin\t插件的安装卸载更新');
 
 	console.log();
 	console.log('please use "light <command> -h" to check the help info;');
