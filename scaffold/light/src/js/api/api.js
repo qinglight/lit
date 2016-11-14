9549702aeb03f46b4f5b329cc9950536d509f102
abel <sdwyub@qq.com>
2016-11-14 10:59:38 +0800

    修改api.js

--- scaffold/light/src/js/api/api.js
+++ scaffold/light/src/js/api/api.js
@@ -1,7 +1,14 @@
-;(function(){
-  API = window.API || {
-    _execute:function(url,params,callback){
-      $.get(url,params,callback);
-    }      
-  };
-})();
+;(function (win) {
+    win.API = {
+
+    };
+
+    var prefix = "";
+    function execute(method,func,cb) {
+        Light.ajax({
+            type:method,
+            url:prefix+func,
+            success:cb
+        })
+    }
+})(window);
